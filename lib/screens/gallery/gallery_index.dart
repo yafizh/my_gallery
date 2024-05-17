041a584f9d386c245a82949c723ef63174ea63d4
yafizh <a.chairil.yafizh@gmail.com>
2024-05-17 23:23:19 +0700

    gallery index interafce

--- lib/screens/gallery/gallery_index.dart
+++ lib/screens/gallery/gallery_index.dart
@@ -9,7 +9,21 @@ class GalleryIndex extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Gallery'),
       ),
-      body: const Text('Gallery Index Page'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 8, crossAxisSpacing: 8),
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(border: Border.all(width: 1)),
+                child: const Center(
+                  child: Text('Image Not Availabel'),
+                ),
+              );
+            }),
+      ),
     );
   }
 }
